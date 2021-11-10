7f1317604fdfd6d1d592af30d53300121252a3b9
PratyayMallik1006 <54743822+PratyayMallik1006@users.noreply.github.com>
2021-11-11 01:12:37 +0530

    Leetcode 20 & 1249

---
+++ Stack/MakeValidParentheses.java
@@ -0,0 +1,18 @@
+//T:O(n)
+//S:O(n)
+class MakeValidParentheses {
+    public String minRemoveToMakeValid(String s) {
+  StringBuilder sb = new StringBuilder(s);
+  Stack<Integer> st = new Stack<>();
+  for (int i = 0; i < sb.length(); ++i) {
+    if (sb.charAt(i) == '(') st.add(i);
+    if (sb.charAt(i) == ')') {
+      if (!st.empty()) st.pop();
+      else sb.setCharAt(i, '*');
+    }
+  }
+  while (!st.empty())
+    sb.setCharAt(st.pop(), '*');
+  return sb.toString().replaceAll("\\*", "");
+}
+}
